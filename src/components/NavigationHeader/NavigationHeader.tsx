88734de736f2135de95437a9ef56e5e42b6294b1
Wesley <wesleyadwebster@gmail.com>
2025-04-28 15:22:37 -0500

    added SVG to Github hyperlink

--- src/components/NavigationHeader/NavigationHeader.tsx
+++ src/components/NavigationHeader/NavigationHeader.tsx
@@ -1,20 +1,32 @@
 import { navLinksConfig } from "../../config/nav/navConfig";
 import classes from './Navigation.module.scss';
 
-const { navLinkContainer } = classes;
+const { navLinkContainer, navLink, navIcon } = classes;
 
 const NavigationHeader = ({ }) => {
     return (
         <nav className={navLinkContainer}>
             <h1>StreamFusion</h1>
-            <div>
+            <div className={navLink}
+            >
                 {
-                    navLinksConfig.map((navLink) => {
+                    navLinksConfig.map((navItem) => {
                         return (
                             <a
-                                key={navLink.navTitle}
-                                href={navLink.path}>
-                                {navLink.navTitle}
+                                key={navItem.navTitle}
+                                href={navItem.path}
+                                className={navLink}
+                            >
+                                {navItem.navTitle}
+                                {navItem.icon && (
+                                    <svg
+                                        className={navIcon}
+                                        viewBox={navItem.icon.viewBox}
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path d={navItem.icon.path}></path>
+                                    </svg>
+                                )}
                             </a>
                         )
                     })
--- src/config/nav/navConfig.ts
+++ src/config/nav/navConfig.ts
@@ -3,10 +3,15 @@ import { NAV_HOME, NAV_GITHUB_TITLE, GITHUB_REPO_LINK } from "../constants/AppCo
 export const navLinksConfig = [
     {
         navTitle: NAV_HOME,
-        path: '/'
+        path: '/',
+        icon: null
     },
     { 
         navTitle: NAV_GITHUB_TITLE,
-        path: GITHUB_REPO_LINK
+        path: GITHUB_REPO_LINK,
+        icon: {
+            viewBox: "0 0 24 24",
+            path: "M22,12v9a1,1,0,0,1-1,1H3a1,1,0,0,1-1-1V3A1,1,0,0,1,3,2h9a1,1,0,0,1,0,2H4V20H20V12a1,1,0,0,1,2,0Zm-.618-9.923A.991.991,0,0,0,21,2H16a1,1,0,0,0,0,2h2.586l-7.293,7.293a1,1,0,1,0,1.414,1.414L20,5.414V8a1,1,0,0,0,2,0V3a1.01,1.01,0,0,0-.077-.382A1,1,0,0,0,21.382,2.077Z"
+        }
     }
 ];
